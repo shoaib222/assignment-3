698ec53cc017e093c2fab877f7d17cbff63f7fd5
mohammed shoaib <shoaibme76@gmail.com>
2023-01-10 23:59:58 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,52 @@
+let myResume={
+    "basics": {
+      "name": "Mohammed shoaib",
+      "email": "Shoaibme76@gmail.com"
+      "phone": 6360122716,
+      "degree": "B.E",
+      "location": {
+        "address": "galib nagar bus stand road Raichur",
+        "postalCode": 584101,
+        "city": "Raichur",
+        "state": "karnataka",
+        "country": "India"
+      },
+      "profiles": [
+        {
+          "website": "https://www.linkedin.com/in/Shoaibme76/",
+          "github":"https://github.com/shoaib222"
+        }
+      ]
+    },
+    "work": [
+      {
+        "Fresher"
+    ],
+    "education": [
+      {
+        "institution": "Navodaya institute of technology",
+        "department": "Mechanical",
+        "studyType": "fulltime",
+        "batch start year": 2014,
+        "batch end year": 2020,
+        "gpa": 6.66,
+      }
+    ],
+    "skills": [
+      {
+        "name": "python,javascript",
+        "level": "beginer",
+      }
+    ],
+    "languages": [
+      {
+        "language": "English","kannada","hindi","telgu",
+      }
+    ],
+    "interests": [
+      {
+        "name": "script writter,music,travel,playing volley ball",
+      }
+    ]
+  }
+  console.log(myResume);
